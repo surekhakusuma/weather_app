93899eca972b9d838ab6aceed9a9633408157601
surekhakusuma <surekhapaul06@gmail.com>
2021-08-13 14:31:09 +0530

    message

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "d6700904ad60ce23df0b8ea134b24f3f"
+    }
+}
+
+module.exports = constants;
